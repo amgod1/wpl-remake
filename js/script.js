b698cd36218f2c092203005f29cc6aea100ab772
Schepuk Kamill <kamillschepuk@gmail.com>
2023-03-28 19:24:31 +0300

    refactor: burger

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 const burger = document.querySelector('.aside-burger')
 const burgerButton = document.querySelector('.burger')
+const closeBurgerButton = document.querySelector('.close-burger')
 
 burgerButton.addEventListener('click',() => {
-  // document.querySelector('.app').addEventListener('click',() => {
-  //   burger.classList.remove('visible')
-  // })
   burger.classList.toggle('visible')
 })
 
+document.addEventListener('click', (event) => {
+  if (!burger.contains(event.target) && !burgerButton.contains(event.target)) {
+    burger.classList.remove('visible');
+  }
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 976 && burger.classList.contains('visible')) {
+    burger.classList.remove('visible');
+  }
+})
+
+closeBurgerButton.addEventListener('click', () => {
+  burger.classList.remove('visible');
+})
